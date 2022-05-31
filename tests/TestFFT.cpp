80d49faefc38aba02dbf66324c0edd55b761121a
Matt Laidlaw <mlaidlaw01@gmail.com>
2022-05-30 20:05:30 -0700

    Add fft smoke test

--- tests/TestFFT.cpp
+++ tests/TestFFT.cpp
@@ -2,60 +2,59 @@
 #include <DFT/FFT.hpp>
 #include "Utils.hpp"
 
-TEST(FFT, Smoke) {
+TEST(FFT, NullInputInvalidParameter) {
     const auto dft = FFT();
 
-    const int numberOfSamples = 8;
-    const int frequency = 1.0;
-    const int amplitude = 1.0;
-
-    const auto in = CreateCompositeSignal(numberOfSamples, {{frequency, amplitude}});
-    auto* out = new complex[numberOfSamples];
-
-    auto result = dft.Forward(in.data(), out, numberOfSamples);
-    EXPECT_EQ(result, DFT::Status::Success);
-
-    PrintSeries(in.data(), numberOfSamples);
-    PrintSeries(out, numberOfSamples);
-
-    for (auto i = 0; i < numberOfSamples; i++) {
-        if (i == frequency) {
-            EXPECT_NEAR(out[i].real(), double(numberOfSamples), 0.0001);
-        } else {
-            EXPECT_NEAR(out[i].real(), 0.0, 0.0001);
-        }
-        EXPECT_NEAR(out[i].imag(), 0.0, 0.0001);
-    }
-
-    delete[] out;
-}
-
-TEST(FFT, NullInputInvalidParameter) {
     const size_t numberOfSamples = 8;
 
     const complex* in = nullptr;
     auto* out = new complex[numberOfSamples];
 
-    const auto dft = FFT();
     auto result = dft.Forward(in, out, numberOfSamples);
-
     EXPECT_EQ(result, DFT::Status::InvalidParameter);
 
     delete[] out;
 }
 
 TEST(FFT, NullOutputInvalidParameter) {
+    const auto dft = FFT();
+
     const size_t numberOfSamples = 8;
 
     const auto* in = new complex[numberOfSamples];
     complex* out = nullptr;
 
-    const auto dft = FFT();
     auto result = dft.Forward(in, out, numberOfSamples);
-
     EXPECT_EQ(result, DFT::Status::InvalidParameter);
 
     delete[] in;
+}
+
+TEST(FFT, SmokeTest) {
+    const auto dft = FFT();
+
+    const int numberOfSamples = 4;
+
+    const auto* in = new complex[numberOfSamples] {{1, 0}, {2, -1}, {0, -1}, {-1, 2}};
+    auto* out = new complex[numberOfSamples];
+
+    auto result = dft.Forward(in, out, numberOfSamples);
+    EXPECT_EQ(result, DFT::Status::Success);
+
+    EXPECT_NEAR(out[0].real(), 2.0, 0.0001);
+    EXPECT_NEAR(out[0].imag(), 0.0, 0.0001);
+
+    EXPECT_NEAR(out[1].real(), -2.0, 0.0001);
+    EXPECT_NEAR(out[1].imag(), -2.0, 0.0001);
+
+    EXPECT_NEAR(out[2].real(), 0.0, 0.0001);
+    EXPECT_NEAR(out[2].imag(), -2.0, 0.0001);
+
+    EXPECT_NEAR(out[3].real(), 4.0, 0.0001);
+    EXPECT_NEAR(out[3].imag(), 4.0, 0.0001);
+
+    delete[] in;
+    delete[] out;
 }
 
 TEST(FFT, Zeros) {
@@ -107,7 +106,7 @@ TEST(FFT, SimpleSignal) {
 TEST(FFT, CompositeSignal) {
     const auto dft = FFT();
 
-    const int numberOfSamples = 8;
+    const int numberOfSamples = 32;
     const auto signals = SignalList{{1, 5}, {3, 3}, {5, 1}};
 
     const auto in = CreateCompositeSignal(numberOfSamples, signals);
@@ -115,8 +114,6 @@ TEST(FFT, CompositeSignal) {
 
     auto result = dft.Forward(in.data(), out, numberOfSamples);
     EXPECT_EQ(result, DFT::Status::Success);
-
-    PrintSeries(out, numberOfSamples);
 
     for (auto i = 0; i < numberOfSamples; i++) {
         if (i == 1) {
